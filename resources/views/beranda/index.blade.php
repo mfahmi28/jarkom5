e9b7a820ed913038a43a3bc778d0d930b1071111
wahyurfldi <rifaldiwahyu3@gmail.com>
2022-12-17 19:01:36 +0700

    Update: Pembatasan Menu Per Role

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -2,36 +2,48 @@
 
 @section('content')
     <div class="grid grid-rows-4 grid-cols-3 gap-7 mt-16">
+        @role("admin|md|supplier")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/produk')">
             <i class="mdi mdi-archive text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Produk</span>
         </div>
+        @endrole
+        @role("admin|md")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/supplier')">
             <i class="mdi mdi-account-group text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Supplier</span>
         </div>
+        @endrole
+        @role("admin|md|admin-cabang|supplier")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/transaksi')">
             <i class="mdi mdi-clipboard-text text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Transaksi</span>
         </div>
+        @endrole
+        @role("admin|md")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/kategori-produk')">
             <i class="mdi mdi-folder text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Kategori</span>
         </div>
+        @endrole
+        @role("admin|md")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/cabang')">
             <i class="mdi mdi-home-assistant text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Cabang</span>
         </div>
-        @role("admin")
+        @endrole
+        @role("admin|md")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/pengguna')">
             <i class="mdi mdi-account-box text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Pengguna</span>
         </div>
         @endrole
+        @role("admin|md|admin-cabang")
         <div class="py-6 px-12 rounded-lg bg-secondary flex flex-row hover:opacity-75 cursor-pointer" onclick="goToMenu('/pengajuan')">
             <i class="mdi mdi-receipt text-purple-100 text-6xl my-auto"></i>
             <span class="text-3xl text-white font-bold my-auto ml-6">Pengajuan</span>
         </div>
+        @endrole
     </div>
 @endsection
 
